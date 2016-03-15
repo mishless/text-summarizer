9aa76945324daad7b0328a792ddd1aa050e255a4
Vukan Turkulov <vukant@gmail.com>
2016-03-15 20:10:33 +0100

    Added batch processing and fixed \n at the beginning of a file.

--- batch_process.py
+++ batch_process.py
@@ -10,11 +10,14 @@ import time
 import argparse
 import nltk.corpus
 import nltk.stem.porter
+import shutil
 
 import textClasses as tc 
 import cluster
 import fuzzy
 import rules
+
+from os import listdir
 
 CUE_PHRASE_FILE = 'bonus_words'
 STIGMA_WORDS_FILE = 'stigma_words'
@@ -23,8 +26,9 @@ def pre_process_text(text):
 
     while text[0] == "\n":
         text = text[1:]
-        
+
     text = text.split('\n', 1)
+
     title = tc.Title(text[0], [])
     text = text[1].replace(u"\u2018", '\'').replace(u"\u2019", '\'').replace(u"\u201c",'"').replace(u"\u201d", '"')
     words = dict()
@@ -39,6 +43,7 @@ def pre_process_text(text):
     tokens = nltk.word_tokenize(title.original)
     tokens = [token for token in tokens if token not in stopwords_list]
     part_of_speech = nltk.pos_tag(tokens)
+
     for (token, word_pos) in zip(tokens, part_of_speech):
         token = token.lower()
         if (token not in words) and (token not in list(string.punctuation) and (token not in stopwords_list)):
@@ -138,8 +143,7 @@ def filter_using_clusters(sentences, percentage, clusters):
             if (len(chosen_sentences) >= number_sentences):
                 break;
     chosen_sentences = sorted(chosen_sentences, key=lambda x: x.position)
-    for sentence in chosen_sentences:
-        print(sentence.original)
+
     return chosen_sentences
 
 def print_based_on_fuzzy(angels_objects, p):
@@ -152,69 +156,120 @@ def print_based_on_fuzzy(angels_objects, p):
         print(sentence.original)
         print("")
 
+def summarize_text(text):
+
+    percentage = 22
+    threads = 4
+    resources = resource_loader()
+
+    preprocessed_text = pre_process_text(text)
+    keyword_feature_value = features.keyword_feature(preprocessed_text[1], preprocessed_text[2])
+    title_word_feature_value = features.title_word_feature(preprocessed_text[0], preprocessed_text[1])
+    sentence_location_feature_value = features.sentence_location_feature(preprocessed_text[1])
+    sentence_length_feature_value = features.sentence_length_feature(preprocessed_text[1])
+    proper_noun_feature_value = features.pos_tag_feature(preprocessed_text[1], preprocessed_text[2], 'NNP')
+    cue_phrase_feature_value = features.phrase_feature(preprocessed_text[1], resources[CUE_PHRASE_FILE])
+    stigma_phrase_feature_value = features.phrase_feature(preprocessed_text[1], resources[STIGMA_WORDS_FILE])
+    numerical_data_feature_value = features.pos_tag_feature(preprocessed_text[1], preprocessed_text[2], 'CD')
+    k_means_result = cluster.k_means(preprocessed_text[1], preprocessed_text[2], percentage, threads)
+    # summary = cluster.cluster_based_summary(preprocessed_text[1], k_means_result[0], k_means_result[1])
+
+    sentences_feature_list = []
+    for (
+keyword_value,
+title_word_value,
+sentence_location_value,
+sentence_lenght_value,
+proper_noun_value,
+cue_phase_value,
+stigma_word_value,
+numerical_data_value,
+) in zip(
+keyword_feature_value,
+title_word_feature_value,
+sentence_location_feature_value,
+sentence_length_feature_value,
+proper_noun_feature_value,
+cue_phrase_feature_value,
+stigma_phrase_feature_value,
+numerical_data_feature_value,
+):
+        sentences_feature_list.append({
+        'keyword': keyword_value,
+        'title_word': title_word_value,
+        'sentence_location': sentence_location_value,
+        'sentence_length': sentence_lenght_value,
+        'proper_noun': proper_noun_value,
+        'cue_phrase': cue_phase_value,
+        'nonessential': stigma_word_value,
+        'numerical_data': numerical_data_value,
+        })
+
+    #fuzzy.print_everything(preprocessed_text[1], sentences_feature_list)
+    fuzzy.set_fuzzy_ranks(preprocessed_text[1], sentences_feature_list)
+    summary_sentences = filter_using_clusters(preprocessed_text[1], float(percentage)/100, k_means_result[1])
+   
+    summary = ""
+    for sentence in summary_sentences:
+        summary += sentence.original
+
+    return summary
+
+#main
+
+def summarize_file(file_name):
+    file = open(file_name)
+    text = file.read()
+    file.close()
+
+    summary = summarize_text(text)
+    return summary
+
+def get_all_file_names(root_name):
+    names = []
+
+    for (roots, dirs, files) in os.walk(root_name):
+        for file in files:
+            names.append(roots+"\\" +  file)
+
+    for i in range(0, len(names)):
+        names[i] = names[i].replace("\\", "/")
+
+    return names
+
 def main():
-    try:
-        start_time = time.time()
+    dir_name = "texts"
+    output_dir = "summaries"
+    problem_dir = "problems"
 
-        processed_input = process_input()
-        text = processed_input['text']
-        percentage = processed_input['percentage']
-        threads = int(processed_input['threads'])
-        resources = resource_loader()
-        preprocessed_text = pre_process_text(text)
-        keyword_feature_value = features.keyword_feature(preprocessed_text[1], preprocessed_text[2])
-        title_word_feature_value = features.title_word_feature(preprocessed_text[0], preprocessed_text[1])
-        sentence_location_feature_value = features.sentence_location_feature(preprocessed_text[1])
-        sentence_length_feature_value = features.sentence_length_feature(preprocessed_text[1])
-        proper_noun_feature_value = features.pos_tag_feature(preprocessed_text[1], preprocessed_text[2], 'NNP')
-        cue_phrase_feature_value = features.phrase_feature(preprocessed_text[1], resources[CUE_PHRASE_FILE])
-        stigma_phrase_feature_value = features.phrase_feature(preprocessed_text[1], resources[STIGMA_WORDS_FILE])
-        numerical_data_feature_value = features.pos_tag_feature(preprocessed_text[1], preprocessed_text[2], 'CD')
-        k_means_result = cluster.k_means(preprocessed_text[1], preprocessed_text[2], percentage, threads)
-        # summary = cluster.cluster_based_summary(preprocessed_text[1], k_means_result[0], k_means_result[1])
+    names = get_all_file_names(dir_name)
 
-        sentences_feature_list = []
-        for (
-    keyword_value,
-    title_word_value,
-    sentence_location_value,
-    sentence_lenght_value,
-    proper_noun_value,
-    cue_phase_value,
-    stigma_word_value,
-    numerical_data_value,
-    ) in zip(
-    keyword_feature_value,
-    title_word_feature_value,
-    sentence_location_feature_value,
-    sentence_length_feature_value,
-    proper_noun_feature_value,
-    cue_phrase_feature_value,
-    stigma_phrase_feature_value,
-    numerical_data_feature_value,
-    ):
-            sentences_feature_list.append({
-            'keyword': keyword_value,
-            'title_word': title_word_value,
-            'sentence_location': sentence_location_value,
-            'sentence_length': sentence_lenght_value,
-            'proper_noun': proper_noun_value,
-            'cue_phrase': cue_phase_value,
-            'nonessential': stigma_word_value,
-            'numerical_data': numerical_data_value,
-            })
+    total = len(names)
+    i = 1
 
-        #fuzzy.print_everything(preprocessed_text[1], sentences_feature_list)
-        fuzzy.set_fuzzy_ranks(preprocessed_text[1], sentences_feature_list)
-        filter_using_clusters(preprocessed_text[1], float(percentage)/100, k_means_result[1])
-        return 0
-    except KeyboardInterrupt:
-        ### handle keyboard interrupt ###
-        return 0
-    except Exception as e:
-        sys.stderr.write(repr(e))
-        traceback.print_tb(e.__traceback__)
-        return 2
+    for name in names:
+        output_name = output_dir + "/" + name.replace(dir_name + "/", "")
+        problem_name = problem_dir + "/" + name.replace(dir_name + "/", "")
 
-if __name__ == "__main__":
-    sys.exit(main())
+        if output_name.replace(output_dir + "/", "") in listdir(output_dir):
+            print("File " + output_name + "already summarized!")
+            i += 1
+            continue
+
+        print("Processing file " + name + " (%d/%d)" % (i,total))
+
+        try:
+            summary = summarize_file(name)
+            file = open(output_name, "w")
+            file.write(summary)
+            file.close()
+            i += 1
+
+        except:
+            shutil.move(name, problem_name)
+            print("Error occured. File " + name + " moved to " + problem_dir + ".")
+            i += 1
+
+
+
+main()
